91ff3ae398af80e6eb279f207524bc699e0f4767
Freigeist <admin@white-it.net>
2015-04-21 13:18:15 +0200

    Update SpielFeld

--- net/white_it/ships/models/Spielfeld.java
+++ net/white_it/ships/models/Spielfeld.java
@@ -4,5 +4,21 @@ package net.white_it.ships.models;
  * @author Florian Tenhaken <admin at white-it dot net>
  */
 public class SpielFeld {
+    /**
+     * Koordinatensystem des Spielfeldes
+     *
+     * @since 1.0
+     */
+    private int[][] coords;
 
+    public SpielFeld(int size) {
+        this.coords = new int[size][size];
+    }
+
+    /**
+     * Gibt das Feld auf der Kommandozeile aus
+     */
+    public void print(){
+
+    }
 }
