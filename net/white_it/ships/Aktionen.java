e60ca217fee06533b7fbd0558dcec830c1adaa5e
aarabaci <arabaci1996@hotmail.de>
2015-04-21 14:05:22 +0200

    Klasse Aktion (Spielvorbereitung)

---
+++ net/white_it/ships/Aktionen.java
@@ -0,0 +1,12 @@
+package net.white_it.ships;
+
+public class Aktionen {
+	/**
+	 * hier werden spieleinstellungen vorgenommen
+	 * 
+	 * @since 1.0
+	 */
+	public static void prepareGame(){
+		
+	}
+}
